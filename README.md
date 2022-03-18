cd0b2727b9e3973c933681546b5ae71d09038bc1
rioj7 <38918937+rioj7@users.noreply.github.com>
2022-03-18 12:02:21 +0100

    v0.1.0 Initial release

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vscode-line-completion
-Visual Studio Code extension that suggests a previous line that starts the same.
+# Line Completion
+
+Suggest a line that starts the same as the current line. 
+
+* the starting whitespace is ignored
+* the comparison is case insensitive (lowercase compare)
+* configure for which files it provides a suggestion
+* configure how many characters it should match minimaly
+
+## Configuration
+
+The extension has the following settings:
+
+* `line-completion.documentSelector` : An array of [DocumentFilter](https://code.visualstudio.com/api/references/vscode-api#DocumentFilter) objects. It determines for which files a suggestion is provided.  
+Default:  
+`[{ "language": "plaintext", "scheme": "file" }]`  
+Ini the documentation you can find a list of [language identifiers](https://code.visualstudio.com/docs/languages/identifiers). There are more language identifiers if you have additional language extensions installed.
+* `line-completion.minimalCharacterCount` : How many characters to type before a completion suggestion is made. (default: `3`)
+
+If you change any setting you have to restart VSCode. This can be done with the command **Developer: Reload Window**
+
+## TODO
+
+* configure workspace relative selector
---
+++ line-completion.js
@@ -0,0 +1,44 @@
+const vscode = require('vscode');
+
+function activate(context) {
+
+  let configuration = vscode.workspace.getConfiguration('line-completion', null);
+  let documentSelector = configuration.get('documentSelector');
+  let minimalCharacterCount = Math.max(configuration.get('minimalCharacterCount'), 1);
+
+  context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+    documentSelector,
+    {
+      /** @param {vscode.TextDocument} document @param {vscode.Position} position */
+      provideCompletionItems(document, position) {
+        const linePrefix = document.lineAt(position).text.trimStart().toLowerCase();
+        if (linePrefix.length < minimalCharacterCount) { return undefined; }
+        const range = new vscode.Range(position.line, position.character - linePrefix.length, position.line, position.character);
+        let result = [];
+        let lineCount = document.lineCount;
+        let currentLine = position.line;
+        loopLine:
+        for (let lineIdx = 0; lineIdx < lineCount; lineIdx++) {
+          if (lineIdx === currentLine) { continue; }
+          const line = document.lineAt(lineIdx).text.trimStart();
+          if (!line.toLowerCase().startsWith(linePrefix)) { continue; }
+          for (const item of result) {
+            if (item.label === line) { continue loopLine; }
+          }
+          let item = new vscode.CompletionItem(line, vscode.CompletionItemKind.Text);
+          item.range = range;
+          result.push(item);
+        }
+        return result;
+      }
+    }
+  ));
+}
+
+function deactivate() {
+}
+
+module.exports = {
+  activate,
+  deactivate
+}
